577d2f439c465b607154f53829df06eee5a1c3f1
Qadir Mohammed-Ali <a.mohammedali@outlook.com>
2017-08-09 23:38:36 -0700

    detect cycle linked list

---
+++ detect_cycle.py
@@ -0,0 +1,11 @@
+def has_cycle(head):
+    fast = head;
+
+    while(fast != None and fast.next != None):
+        fast = fast.next.next;
+        head = head.next;
+
+        if(head == fast):
+            return True;
+
+    return False;
